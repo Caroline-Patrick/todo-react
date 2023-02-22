b8a6335f04498a9517b4e8290f86e7a2764fabb8
Caroline-Patrick <carolinespatrick@gmail.com>
2023-02-21 21:31:26 -0600

    added delete buttons

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,6 +2,8 @@
 // object destructuring --> 
 
 import React, { Component } from 'react'
+import { nanoid } from 'nanoid';
+
 
 export default class Todos extends Component {
     constructor(props){
@@ -19,11 +21,28 @@ export default class Todos extends Component {
         // console.log("clicked", this.state.isClicked)
         this.setState({
             // isClicked: !this.state.isClicked
-            todos: [...this.state.todos, this.state.text],
+            todos: [...this.state.todos, {id: nanoid(), text: this.state.text}],
             //copy original array and then add whatever is inputted, so that the OG array isn't mutated
             text:""
-            
+            // delete user's input from the input bar
         })
+    }
+
+    handleDelete =(id) => {
+      
+//find item by id to get its index
+      const index = this.state.todos.findIndex(todo => todo.id ===id) 
+
+      // create copy of state so we don't mutate directly
+      const copy = [...this.state.todos]
+      
+      //mutate the copy
+      copy.splice(index, 1);
+      
+      //set the state
+      this.setState({
+        todos: copy
+      })
     }
   // any time state changes, everything in render will run
     render() {
@@ -34,16 +53,21 @@ export default class Todos extends Component {
     
         <input onChange={(event)=> this.setState({
             text: event.target.value})} 
+            // event is the onChange object occurring that has target on it as an object, and value is a property of the target object (if console.log(event) you can see)
             value={this.state.text}
         />
         <button onClick={this.handleClick}>Add ToDo</button>
-        {
-            this.state.todos.map((todo, index)=>{
+        <ul>
+        {this.state.todos.map((todo)=>{
                 return (
-                    <h2>{todo}</h2>
+                    <li className="list-item" key={todo.id}>
+                    <h4>{todo.text}</h4>
+                    <button id="delete-button" onClick={()=> this.handleDelete(todo.id)}>X</button>
+                    </li>
                 )
-            })
-        }
+            })}
+            </ul>
+        
       </div>
     );
   }
